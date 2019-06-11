4acfb6e4b6aae0008a01460c93e314edfdd0cc68
TimeSmash <TimeSmash@github.com>
2019-06-11 14:52:47 -0400

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,19 @@
-class Teacher
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def teach
+    KNOWLEDGE.sample
+  end
   
 end
